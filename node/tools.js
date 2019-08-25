34049629988c85361904c963d274261effbc6f20
axlecho <axlecho@gmail.com>
2019-08-25 20:34:17 +0800

    [feature] add network function

--- node/tools.js
+++ node/tools.js
@@ -1,15 +1,17 @@
 var program = require('commander')
 var cheerio = require('cheerio')
-var requirejs = require('requirejs');
+var requirejs = require('requirejs')
+var request = require('sync-request')
+var find = require('cheerio-eq');
 var fs = require('fs')
 
 program
   .option('-b, --base <type>', '插件路径')
 
-program.parse(process.argv);
+program.parse(process.argv)
 
 if (program.base) {
-	console.log('插件路径 --> ' + program.base);
+	console.log('插件路径 --> ' + program.base)
 } else {
 	console.log('请输入插件路径')
 }
@@ -23,26 +25,38 @@ function checkSite() {
 }
 
 
-function testApi() {
-	var jsoup = {}
-	var log = {}
+var testPage = request('GET','https://so.177mh.net/k.php?k=%E8%BE%89%E5%A4%9C').getBody('utf8')
+// console.log(testPage)
+// var testPage = fs.readFileSync(program.base + '/test.html')
+var api = require(program.base + '/parser.js')
+var root = cheerio.load(testPage)
 
-	requirejs.config({
-		paths:{
-        	"parse": program.base + '/parser'
-    	},
-	    //Pass the top-level main.js/index.js require
-	    //function to requirejs so that node modules
-	    //are loaded relative to the top-level JS file.
-	    nodeRequire: require
-	});
 
-	requirejs(['parse'], function   () {
-		// console.log(jsoup)
-	});
+// console.log(root('ul.new_hits_ul li'))
+
+
+function DomNode(node) {
+    this.html = function(cssQuery) { return (cssQuery === undefined ? node.html() : find(node,cssQuery).html())}
+    this.text = function(cssQuery) { return (cssQuery === undefined ? node.text() : find(node,cssQuery).text())}
+    this.src  = function(cssQuery) { return (cssQuery === undefined ? node.src() : find(node,cssQuery).attr('src'))}
+    this.href = function(cssQuery) { return (cssQuery === undefined ? node.href() : find(node,cssQuery).attr('href'))}
+    this.attr = function(attr,cssQuery) { return (cssQuery ===undefined ? node.attr(attr) : find(node,cssQuery).attr(attr)) }
+    // Warning this function return a java object
+    this.list = function(cssQuery) { 
+        var result = []
+        for (var i = 0; i < node(cssQuery).length ; i++) {
+            result.push(new DomNode(cheerio.load(node(cssQuery).get(i))))
+        }
+        return result
+    }
 }
 
-testApi()
-var a = requirejs('parse')
-console.log('--------------------')
-console.log(a)
+api.console = console
+api.doc = new DomNode(root)
+
+// console.log(JSON.parse(api.data()))
+// console.log(JSON.parse(api.top()))
+console.log(JSON.parse(api.search()))
+// console.log(JSON.parse(api.info('237146')))
+
+
--- test/parser.js
+++ test/parser.js
@@ -1,8 +1,15 @@
-var jsoup = {}
-var log = {}
-var console = {
-    log : function(msg) { log.println(msg)}
+
+ // var api.console = {   log : function(msg) { log.println(msg)} }
+
+var api = {
+    console : {},
+    doc : {},
+    info : function(gid) {return info(gid)},
+    top : function() {return _top()},
+    data : function() {return data()},
+    search : function() {return search()}
 }
+
 
 function DomNode(node) {
     this.html = function(cssQuery) { return (cssQuery === undefined ? node.html() : node.html(cssQuery)) + ''};
@@ -20,7 +27,7 @@ function DomNode(node) {
     };
 }
 
-var doc = new DomNode(jsoup)
+// var api.doc = new DomNode(jsoup)
 
 function parseTitle() {
     return jsoup.text('ul.ar_list_coc li:eq(0)') + '';
@@ -31,17 +38,17 @@ function info(gid) {
     var result = {};
     result.info = {};
     result.info.gid = gid + '';
-    result.info.title =  doc.text('ul.ar_list_coc li:eq(0)');
+    result.info.title =  api.doc.text('ul.ar_list_coc li:eq(0)');
     result.info.titleJpn = '';
-    result.info.posted = doc.text('ul.ar_list_coc li:eq(4)').replace(/[^0-9]/ig,'');
-    result.info.thumb = doc.src('div.ar_list_coc img');
-    result.info.uploader =  doc.text('ul.ar_list_coc li:eq(1) a'); 
-    result.intro = doc.text('i#det') + '';
-    result.chapterCount = doc.text('ul.ar_list_coc li:eq(3)').replace(/[^0-9]/ig,'');
+    result.info.posted = api.doc.text('ul.ar_list_coc li:eq(4)').replace(/[^0-9]/ig,'');
+    result.info.thumb = api.doc.src('div.ar_list_coc img');
+    result.info.uploader =  api.doc.text('ul.ar_list_coc li:eq(1) a'); 
+    result.intro = api.doc.text('i#det') + '';
+    result.chapterCount = api.doc.text('ul.ar_list_coc li:eq(3)').replace(/[^0-9]/ig,'');
     result.favoriteCount = 0;
     result.chapters = [];
 
-    doc.list('ul.ar_rlos_bor li').forEach(function(v,i) {
+    api.doc.list('ul.ar_rlos_bor li').forEach(function(v,i) {
         var chapter = {};
         chapter.title = v.text('a');
         chapter.url = v.href('a');
@@ -50,78 +57,69 @@ function info(gid) {
     
     result.comments = [];
     updateTime = 0;
-    
-    // console.log(result);
     return JSON.stringify(result);
 };
 
 function recent() {
     var result = {};
     result.datas = [];
-    doc.list('ul.new_hits_ul li').forEach(function(v,i){
+    api.doc.list('ul.new_hits_ul li').forEach(function(v,i){
         var item = {};
         item.gid = v.href('a').replace(/[^0-9]/ig,'');
         item.title = v.text('a');
         item.thumb = v.src('img');
-        console.log(item);
+        api.console.log(item);
         result.datas.push(item);
     });
     result.pages = 1;
     result.currentPage = 1;
     
-    console.log(result);
+    api.console.log(result);
     return JSON.stringify(result); 
 }
 
 function _top() {
     var result = {};
     result.datas = [];
-    doc.list('ul.new_hits_ul li').forEach(function(v,i){
+    api.doc.list('ul.new_hits_ul li').forEach(function(v,i){
         var item = {};
         item.gid = v.href('a').replace(/[^0-9]/ig,'');
         item.title = v.text('a');
         item.thumb = v.src('img');
-        console.log(item);
+        api.console.log(item);
         result.datas.push(item);
     });
     result.pages = 1;
     result.currentPage = 1;
-    
-    console.log(result);
     return JSON.stringify(result);
 };
 
 function search() {
     var result = {};
     result.datas = [];
-    doc.list('div.ar_list_co ul dl').forEach(function(v,i){
+    api.doc.list('div.ar_list_co ul dl').forEach(function(v,i){
         var item = {};
         item.gid = v.href('a').replace('https://www.177mh.net','').replace(/[^0-9]/ig,'');
         item.title = v.text('h1');
         item.thumb = v.src('img');
         item.uploader = v.text('i.author a:eq(1)')
-        console.log(item);
         result.datas.push(item);
     });
-    result.pages = doc.text('div.pages_s').split('/')[1].replace(/[^0-9]/ig,'');
-    result.currentPage = doc.text('div.pages_s').split('/')[0].replace(/[^0-9]/ig,'');
-
-    console.log(result);   
+    result.pages = api.doc.text('div.pages_s').split('/')[1].replace(/[^0-9]/ig,'');
+    result.currentPage = api.doc.text('div.pages_s').split('/')[0].replace(/[^0-9]/ig,'');
     return JSON.stringify(result);   
 };
 
 function data() {
     var result = {};
     result.data = [];
-    console.log('=================')
-    var script = doc.html('script')
-    console.log(script)
-
+    var script = api.doc.html('script')
     eval(script);
     var paths = msg.split('|');
     for (var i = 0; i < paths.length; i++) {
         result.data.push('https://hws.readingbox.net/h' + img_s + '/' + paths[i] + '.webp')
     }
-    // console.log(result);   
     return JSON.stringify(result);
 };
+
+module.exports = api
